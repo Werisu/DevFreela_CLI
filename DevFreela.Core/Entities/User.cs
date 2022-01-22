20e3ab1f470d8b48982f0eb0956aa0fcd28fa69b
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-01-22 11:53:11 -0300

    Camada Core (Domain)
    Implementando a camada de Dominio. Iniciando Ccriação das Entidades, Enums, Exceptions e Repositories

---
+++ DevFreela.Core/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullName, string email, DateTime birthDate)
+        {
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+            CreatedAt = DateTime.Now;
+            Active = true;
+
+            Skills = new List<UserSkill>();
+            OwnedProject = new List<Project>();
+            FreelancerProject = new List<Project>();
+        }
+
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool Active { get; set; }
+
+        // relação de N para N = nova entidade
+        public List<UserSkill> Skills { get; private set; }
+        public List<Project> OwnedProject { get; private set; }
+        public List<Project> FreelancerProject { get; private set; }
+
+
+    }
+}
---
+++ DevFreela.Core/Enums/ProjectStatusEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DevFreela.Core.Enums
+{
+    public enum ProjectStatusEnum
+    {
+        Created = 0,
+        InProgress = 1,
+        Suspended = 2,
+        Cancelled = 3,
+        Finished = 4
+    }
+}
